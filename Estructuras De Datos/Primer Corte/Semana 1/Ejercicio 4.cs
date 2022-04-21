7d673f80d51dd2d14df7b23bf46bb3caa60a024d
SebastianQ0115 <99778409+SebastianQ0115@users.noreply.github.com>
2022-04-20 21:35:00 -0500

    Add files via upload

---
+++ Estructuras De Datos/Primer Corte/Semana 1/Ejercicio 4.cs
@@ -0,0 +1,38 @@
+using System;
+using System.linq;
+using System.collections;
+using System.collections.Generic;
+
+namespace collazs
+{
+    class program 
+    {
+        static void Main(string[]args)
+        {   
+           
+          int numero1,fase1 = 0;
+          console.writeLine("dame un numero ");  
+          numero = console.ReadLine();
+          numero1 = numero;
+          while(numero1>1){
+           if (numero1%2==0);
+         { 
+            numero1 =numero1/2; 
+            console.WriteLine(" es{0} ",numero1);
+          }
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+    }
